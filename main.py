2f9077138ceb3028cbdda3bd56ac57c255a05eb8
Nick-Shpuntikov <sna1999@yandex.ru>
2022-03-12 23:08:54 +0300

    С панелью

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='static')
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('ecg.html')
+
 if __name__ == '__main__':
     app.run(host='localhost', debug=True)

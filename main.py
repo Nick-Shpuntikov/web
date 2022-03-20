ab752e7ddd859165653421fd15cc7479d1034874
Nick-Shpuntikov <sna1999@yandex.ru>
2022-03-20 12:49:50 +0300

    Сделал структуру по шаблонизатору, js по прежнему не работает

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__, template_folder='static')
+app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+@app.route('/home')
 def home():
     return render_template('ecg.html')
+
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/products')
+def products():
+    return render_template('products.html')
+
 
 if __name__ == '__main__':
     app.run(host='localhost', debug=True)
